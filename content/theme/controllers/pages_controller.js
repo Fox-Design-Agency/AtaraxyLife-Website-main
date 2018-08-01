84db772e228a59ed71d033544abeabcb5aaf5cbd
Christopher Fox <cfox1290@gmail.com>
2018-08-01 14:20:17 -0600

    intial Aristos 0.1.0 update

--- content/theme/controllers/pages_controller.js
+++ content/theme/controllers/pages_controller.js
@@ -1,13 +1,13 @@
-// Page model queries
+/* Page model queries */
 const FindPageWithParam = require("../../../important/admin/adminModels/queries/page/FindPageWithParam");
-// media queries
+/* media queries */
 const FindAllMedia = require("../../../important/admin/adminModels/queries/media/FindAllMedia");
 module.exports = {
   home(req, res, next) {
     const AllMedia = FindAllMedia();
     const HomePage = FindPageWithParam({ slug: "home" });
     Promise.all([AllMedia, HomePage]).then(result => {
-      res.render("../views/index", {
+      res.render(`${result[1][0].template}`, {
         title: result[1][0].title,
         content: result[1][0].content,
         keywords: result[1][0].keywords,
@@ -16,7 +16,7 @@ module.exports = {
         media: result[0]
       });
     });
-  }, // end of home function
+  } /* end of home function */,
 
   anyPage(req, res, next) {
     let slug = req.params.slug;
@@ -26,15 +26,26 @@ module.exports = {
       if (result[1].length < 1) {
         res.redirect("/");
       } else {
-        res.render("index", {
-          title: result[1].title,
-          content: result[1].content,
-          keywords: result[1].keywords,
-          description: result[1].description,
-          author: result[1].author,
-          media: result[0]
-        });
+        if (typeof result[1][0].template !== "undefined") {
+          res.render(`${result[1][0].template}`, {
+            title: result[1][0].title,
+            content: result[1][0].content,
+            keywords: result[1][0].keywords,
+            description: result[1][0].description,
+            author: result[1][0].author,
+            media: result[0]
+          });
+        } else {
+          res.render("index", {
+            title: result[1][0].title,
+            content: result[1][0].content,
+            keywords: result[1][0].keywords,
+            description: result[1][0].description,
+            author: result[1][0].author,
+            media: result[0]
+          });
+        }
       }
     });
-  } // end of any page function
+  } /* end of any page function */
 };
